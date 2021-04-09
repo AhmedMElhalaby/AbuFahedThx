5a36687b743bc1d04fc676d156fcd8227ccfbe96
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-04-09 14:21:46 +0300

    update

--- resources/views/certification.blade.php
+++ resources/views/certification.blade.php
@@ -80,10 +80,10 @@
 
                 </td>
                 <td style="width: 50%;height: 150px;padding-right: 75px;text-align: center">
-                    <p style="text-align: center">
+                    <p style="text-align: center;padding-top: 35px">
                         <img src="{{asset($Objects['signature'])}}" style="width: 185px;height: 85px;padding-top: -55px;" alt="">
                     </p>
-                    <p style="text-align: center">
+                    <p style="text-align: center;">
                         <span style="color:#31706C;font-size: 25px;padding-top: -55px;">{!! $Objects['signature_name'] !!}</span><br>
                     </p>
                 </td>
